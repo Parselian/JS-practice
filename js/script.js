06a3f759c1334f67477ef8257a33e32afc45c48f
jells <jells@jells.jells>
2020-05-25 12:16:49 +0300

    сделал тоггл выпадающего списка, добавление и удаление полей ввода

--- js/script.js
+++ js/script.js
@@ -2,23 +2,44 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   const preloader = document.querySelector('.preloader'),
-        guestCalc = document.querySelector('.guest-calc'),
+				guestCalc = document.querySelector('.guest-calc'),
+				guestCalcWrap = guestCalc.querySelector('.guest-calc__wrap'),
         calcTitle = guestCalc.querySelector('.guest-calc__title'),
+        calcItem = guestCalc.querySelector('.guest-calc__item'),
         calcDropdown = guestCalc.querySelector('.guest-calc__dropdown'),
         calcButtons = guestCalc.querySelector('.guest-calc__buttons');
         
   function hidePreloader() {
     let timerId = setTimeout(() => {
       preloader.classList.remove('preloader_visible');
-    }, 1000);
+    }, 10);
   }
   hidePreloader();
 
   const controlInputButtons = () => {
     calcDropdown.remove();
+
+    function removeItemInput(target) {
+			let parentBlock = target.closest('.guest-calc__item'),
+					parentSibling = parentBlock.nextElementSibling;
+
+			if (parentSibling && parentSibling.matches('.guest-calc__dropdown')) {
+				parentBlock.nextElementSibling.remove();
+			}
+
+			target.closest('.guest-calc__item').remove();
+			
+    }
+
+    function addItemInput() {
+			let clonedItem = calcItem.cloneNode('true');
+
+      guestCalcWrap.insertAdjacentElement('beforeend', clonedItem);
+    }
     
     function hideAllInputButtons() {
-      const calcItems = guestCalc.querySelectorAll('.item-input__arrow')
+      const calcItems = guestCalc.querySelectorAll('.item-input__arrow');
+
       calcItems.forEach(item => {
         item.classList.add('item-input_hidden');
       });
@@ -29,25 +50,32 @@ window.addEventListener('DOMContentLoaded', () => {
       target.previousElementSibling.classList.remove('item-input_hidden');
     }
 
-    function changeInputValue(target) {
-      if (target.matches('.item-input__plus')) {
-        target.previousElementSibling.value++;
-        showDropdown(target);
+    function changeInputValue(target, maxGuests) {
+      let prevElemNeighbor = target.previousElementSibling,
+          nextElemNeighbor = target.nextElementSibling;
+
+      if (target.matches('.item-input__plus') && prevElemNeighbor.value < maxGuests) {
+        prevElemNeighbor.value++;
       } else if (target.matches('.item-input__minus') && 
-      target.nextElementSibling.value > 0) {
-          target.nextElementSibling.value--;
+      nextElemNeighbor.value > 0) {
+				nextElemNeighbor.value--;
       }
     }
 
-    function showDropdown(target) {
+    function toggleDropdown(target) {
       let parentElem = target.parentElement.parentElement,
+          parentNeighbor = parentElem.nextElementSibling,
+          inputChildren = target.parentElement.querySelector('.item-input'),
           clonedDropdown = calcDropdown.cloneNode(true);
 
-      if (!parentElem.nextElementSibling) {
-        parentElem.insertAdjacentElement('afterend', clonedDropdown);
-      }
+      if (parentNeighbor !== clonedDropdown && inputChildren.value > 0 && inputChildren.matches('.input_children')) {
 
-      
+        parentElem.insertAdjacentElement('afterend', clonedDropdown);
+			} else if (parentNeighbor && inputChildren.value <= 0 && 
+			parentNeighbor.matches('.guest-calc__dropdown') && inputChildren.matches('.input_children')) {
+
+        parentNeighbor.remove();
+      }
     }
 
     //controlling inputs
@@ -62,7 +90,12 @@ window.addEventListener('DOMContentLoaded', () => {
       if (target.matches('.item-input')) {
         showInputButtons(target);
       } else if (target.matches('.item-input__arrow')) {
-        changeInputValue(target);
+        changeInputValue(target, 3);
+        toggleDropdown(target);
+      } else if (target.closest('.guest-calc__item-remove')) {
+        removeItemInput(target);
+      } else if (target.matches('#button-add')) {
+        addItemInput();
       }
     });
   };
